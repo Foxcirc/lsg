24af2a502b4e2b8f7ff78d0c1f4b2197ab25b7ad
foxcirc <not-provided>
2026-01-10 01:56:58 +0100

    trying out event dispatching approaches

--- crates/framework/src/lib.rs
+++ crates/framework/src/lib.rs
@@ -1,7 +1,7 @@
 
 #![doc(html_logo_url = "https://raw.githubusercontent.com/Foxcirc/lsg/main/docs/icon.png")]
 
-use std::{rc::Rc, sync::Arc};
+use std::{future, mem, sync::{Arc, Mutex}, task};
 
 use futures_lite::future::block_on;
 
@@ -9,7 +9,7 @@ use futures_lite::future::block_on;
 mod test;
 
 pub struct App {
-    executor: Rc<async_executor::LocalExecutor<'static>>,
+    executor: Arc<async_executor::LocalExecutor<'static>>,
 }
 
 impl App {
@@ -24,10 +24,10 @@ impl App {
             desktop::EventLoop::<()>::run(config, |el| {
 
                 let this = Self {
-                    executor: Rc::new(async_executor::LocalExecutor::new()),
+                    executor: Arc::new(async_executor::LocalExecutor::new()),
                 };
 
-                let executor2 = Rc::clone(&this.executor);
+                let executor2 = Arc::clone(&this.executor);
 
                 // This task pumps events and calls the event handlers.
                 executor2.spawn(async move {
@@ -47,17 +47,130 @@ impl App {
 }
 
 pub struct Window {
+    dispatcher: EventDispatcher<WindowEventHandler>,
 }
 
 impl Window {
 
     pub fn new(app: &App) -> Self {
 
-        Self {}
+        Self {
+            dispatcher: EventDispatcher::new(),
+        }
+
+    }
+
+    pub async fn closed(&self) {
+
+        let key = self.dispatcher.register(WindowEventHandler::Closed(None));
+
+        self.dispatcher.listen(
+            key, |h| if let WindowEventHandler::Closed(it) = h { it.take() } else { unreachable!() }
+        ).await
+
+    }
+
+    fn handle(&self, event: desktop::WindowEvent) {
+
+        self.dispatcher.notify(|handler| match (handler, &event) {
+            (WindowEventHandler::Closed(it), desktop::WindowEvent::Close) => { *it = Some(()); true }
+            (..) => false
+        });
 
     }
 
 }
+
+enum WindowEventHandler {
+    Closed(Option<()>),
+}
+
+struct EventDispatcher<H> {
+    handlers: Mutex<slab::Slab<EventHandler<H>>>,
+}
+
+impl<H> EventDispatcher<H> {
+
+    pub fn new() -> Self {
+        Self {
+            handlers: Mutex::new(slab::Slab::new())
+        }
+    }
+
+    /// Register a new event handler.
+    pub fn register(&self, inner: H) -> usize {
+        let mut guard = self.handlers.lock().unwrap();
+        guard.insert(EventHandler { waker: None, inner })
+    }
+
+    pub async fn listen<F, T>(&self, key: usize, mut access: F) -> T
+        where F: FnMut(&mut H) -> Option<T> {
+
+            future::poll_fn(move |cx| {
+
+                let mut guard = self.handlers.lock().unwrap();
+                let handler = &mut guard[key];
+
+                let event = access(&mut handler.inner);
+
+                if let Some(it) = event {
+
+                    task::Poll::Ready(it)
+
+                } else {
+
+                    match &mut handler.waker {
+                        Some(it) => it.clone_from(cx.waker()),
+                        None => handler.waker = Some(cx.waker().clone())
+                    }
+
+                    task::Poll::Pending
+
+                }
+
+            }).await
+
+    }
+
+    pub fn notify<F>(&self, mut assess: F)
+        where F: FnMut(&mut H) -> bool {
+
+        let mut guard = self.handlers.lock().unwrap();
+
+        for (.., handler) in &mut *guard {
+
+            let modified = assess(&mut handler.inner);
+
+            if let Some(it) = &handler.waker && modified {
+                it.wake_by_ref();
+            }
+
+        }
+
+    }
+
+}
+
+struct EventHandler<H> {
+    waker: Option<task::Waker>,
+    inner: H,
+}
+
+// enum EventFilter<T> {
+//     Any,
+//     One(mem::Discriminant<T>),
+//     Multiple(Vec<mem::Discriminant<T>>),
+// }
+
+// impl<T> EventFilter<T> {
+//     pub fn matches(&self, value: &T) -> bool {
+//         let discr = mem::discriminant(value);
+//         match self {
+//             Self::Any => true,
+//             Self::One(it) => v
+//         }
+//     }
+// }
 
 /*
 
